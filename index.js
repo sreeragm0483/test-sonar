51fa248617f66fb84178116432686f751a6b1f25
sreeragm0483 <162032237+sreeragm0483@users.noreply.github.com>
2024-09-02 15:03:24 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+const CALLBACK_REGEX = new RegExp('(^$)|((.+:\\/\\/.*)(d*)\\/?(.*))');
+const SUBDOMAIN_REGEX = /^(([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+)(:\d+)?(\/\S*)?$/;
+const CALLBACK_REGEX = /(^$)|((.+:\/\/.*)(d*)\/?(.*))/;
