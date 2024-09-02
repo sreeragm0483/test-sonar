e837e3f12800614733479657709fc1f6e507af2f
sreeragm0483 <162032237+sreeragm0483@users.noreply.github.com>
2024-09-02 15:16:24 +0530

    latest regex index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// const SUBDOMAIN_REGEX = /^(([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+)(:\d+)?(\/\S*)?$/;
+// const URL_REGEX = new RegExp('(^$)|((.+:\\/\\/.*)(d*)\\/?(.*))');
+// const NEW_CALLBACK_REGEX = /^(?:.+:\/\/.*)(d*)\/?(.*)$/;
+
+
+
+
+const CALLBACK_REGEX = /^(?:.+:\/\/.*)(d*)\/?(.*)$/;
 const SUBDOMAIN_REGEX = /^(([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+)(:\d+)?(\/\S*)?$/;
-const URL_REGEX = new RegExp('(^$)|((.+:\\/\\/.*)(d*)\\/?(.*))');
-const NEW_CALLBACK_REGEX = /^(?:.+:\/\/.*)(d*)\/?(.*)$/;
-
-
